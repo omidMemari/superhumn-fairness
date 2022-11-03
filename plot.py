bf12103410c893aec23b475ddced7fc755ebc314
linh <lvu5@uic.edu>
2022-11-03 01:15:29 -0500

    test plot

--- plot.py
+++ plot.py
@@ -1,13 +1,14 @@
 
 import os
+import pickle
 from main import Super_human, make_experiment_filename, load_object
-
+import matplotlib.pyplot as plt
 # read files in the experiment/test
 # read demo_list.pickle
 #f = open("./experiments/test/Adult_0-05_0-05_100")
 
 feature = {0: "ZeroOne", 1: "Demographic parity difference", 2: "False negative rate difference", 3: "False positive rate difference", 4: "Equalized odds difference"}
-
+features = {0: "Demographic parity difference", 1: "False positive rate difference", 2: "False negative rate difference", 3: "Equalized odds difference", 4: "ZeroOne"}
 root = "experiments"
 test_path = os.path.join(root,"test")
 
@@ -19,25 +20,40 @@ dataset = "Adult"
 num_of_demos = 100
 num_of_features = 5
 
-def read_demo_list():
-    with open('demo_list.pickle', 'rb') as handle:
-      demo_list = pickle.load(handle)
-    return demo_list
-
-
 # look at read_model_from_file(self) from main.py to get an idea of how to read experiment file and base_model.pickel and demo_list.pickle
 
 experiment_filename = make_experiment_filename(dataset = dataset, lr_theta = lr_theta, lr_alpha = lr_alpha, num_of_demos = num_of_demos)
 file_dir = os.path.join(test_path)
 model_params = load_object(file_dir,experiment_filename)
 
-print(model_params)
 
 sh_obj = Super_human(dataset = dataset, num_of_demos = num_of_demos, num_of_features = num_of_features)
 
 demo_list = sh_obj.read_demo_list()
 
+zero_ones = []
+dp = []
+fnr = []
+fpr = []
+eq_odds = []
 print(demo_list[0].metric)
-
-print(model_params['eval_sh'].loc['Demographic parity difference'])
+for i in range(len(demo_list)):
+    zero_ones.append(demo_list[i].metric[0])
+    dp.append(demo_list[i].metric[1])
+    fnr.append(demo_list[i].metric[2])
+    fpr.append(demo_list[i].metric[3])
+    eq_odds.append(demo_list[i].metric[4])
     
+print(model_params['eval_sh'])
+zero_one_sh = model_params['eval_sh'].loc[feature[0]]
+dp_sh = model_params['eval_sh'].loc[feature[1]]
+fnr_sh = model_params['eval_sh'].loc[feature[2]]
+fpr_sh = model_params['eval_sh'].loc[feature[3]]
+eq_odds_sh = model_params['eval_sh'].loc[feature[4]]
+
+
+# plot zero_ones as y-axis and dp as x-axis in green color
+# plot zero_one_sh as y-axis and dp_sh as x-axis in red color
+plt.plot(dp, zero_ones, 'go', label = 'demo_list')
+plt.plot(dp_sh, zero_one_sh, 'ro', label = 'sh')
+plt.savefig("zeroOne vs Dp.png")
