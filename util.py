b1b332a4557bcacf8112602eff8e6fe81b7dc39e
omemar2 <omid.memarrast@gmail.com>
2022-12-16 13:41:56 -0600

    added ppv diff in util.py

--- util.py
+++ util.py
@@ -4,12 +4,27 @@ import pandas as pd
 from fairlearn.metrics import (
     MetricFrame,
     selection_rate, demographic_parity_difference, demographic_parity_ratio,
-    false_positive_rate, false_negative_rate,
+    false_positive_rate, false_negative_rate, true_negative_rate, true_positive_rate,
     false_positive_rate_difference, false_negative_rate_difference,
     equalized_odds_difference)
 from sklearn.metrics import balanced_accuracy_score, roc_auc_score, zero_one_loss
 
-
+def positive_predictive_value(y_true, y_pred, group):
+    # PPV for every group
+    for group_value in group.unique():
+        # Get the indices for the group
+        group_indices = group == group_value
+        # Get the true positives
+        true_positives = np.logical_and(y_true[group_indices], y_pred[group_indices])
+        # Get the false positives
+        false_positives = np.logical_and(np.logical_not(y_true[group_indices]), y_pred[group_indices])
+        # Calculate the PPV
+        ppv = np.sum(true_positives) / (np.sum(true_positives) + np.sum(false_positives))
+        print(f"PPV for {group.name}={group_value}: {ppv}")
+    # get difference in PPV
+    ppv_diff = np.abs(ppv[0] - ppv[1])
+    return ppv_diff
+    
 
 # Helper functions
 def get_metrics_df(models_dict, y_true, group):
@@ -32,7 +47,9 @@ def get_metrics_df(models_dict, y_true, group):
         "Equalized odds difference": (
             lambda x: equalized_odds_difference(y_true, x, sensitive_features=group), True),
         "ZeroOne": (
-            lambda x: zero_one_loss(y_true, x), True)
+            lambda x: zero_one_loss(y_true, x), True),
+        "positive_predictive_value_difference": (
+            lambda x: positive_predictive_value(y_true, x, group), True)
         #"Overall AUC": (
         #    lambda x: roc_auc_score(y_true, x), False),
         #"AUC difference": (
@@ -43,3 +60,20 @@ def get_metrics_df(models_dict, y_true, group):
         df_dict[metric_name] = [metric_func(preds) if use_preds else metric_func(scores) 
                                 for model_name, (preds, scores) in models_dict.items()]
     return pd.DataFrame.from_dict(df_dict, orient="index", columns=models_dict.keys())
+
+
+
+
+g = ["m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f"] #np.concatenate((np.ones(27), np.zeros(18)), axis=0)
+print(g)
+y_true = list(np.concatenate((np.ones(9), np.zeros(18), np.ones(9), np.zeros(9)), axis=0))
+y_pred = list(np.concatenate((np.ones(3), np.zeros(6), np.ones(3), np.zeros(15), np.ones(2), np.zeros(7), np.ones(2), np.zeros(7)), axis=0))
+
+
+ 
+
+#df_g = pd.DataFrame(g, columns=['gender'])
+
+ppv = positive_predictive_value(y_true, y_pred, g)
+
+print(ppv)
