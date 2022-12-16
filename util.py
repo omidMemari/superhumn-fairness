07bccbc6606a199f521ac92d261a06505b9d68a6
linh <lvu5@uic.edu>
2022-12-16 16:09:06 -0600

    tetst

--- util.py
+++ util.py
@@ -7,23 +7,50 @@ from fairlearn.metrics import (
     false_positive_rate, false_negative_rate, true_negative_rate, true_positive_rate,
     false_positive_rate_difference, false_negative_rate_difference,
     equalized_odds_difference)
-from sklearn.metrics import balanced_accuracy_score, roc_auc_score, zero_one_loss
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score, zero_one_loss, confusion_matrix
+
+
+def true_positives(y_true, y_pred):
+    tp = 0
+    for i in range(len(y_true)):
+        if y_true[i] == 1 and y_pred[i] == 1:
+            tp += 1
+    return tp
+
+
+def false_positives(y_true, y_pred):
+    fp = 0
+    for i in range(len(y_true)):
+        if y_true[i] == 0 and y_pred[i] == 1:
+            fp += 1
+    return fp
+
+# backup
+# def positive_predictive_value_helper(y_true, y_pred):
+#     tp = np.sum(np.logical_and(y_true, y_pred))
+#     fp = np.sum(np.logical_and(np.logical_not(y_true), y_pred))
+#     return tp / (tp + fp)
+
+
+# def positive_predictive_value_helper(y_true, y_pred):
+#     confusion_matrix = confusion_matrix(y_true, y_pred)
+#     tp = confusion_matrix[1][1]
+#     fp = confusion_matrix[0][1]
+#     return tp / (tp + fp)
+
+
+
+
+def positive_predictive_value_helper(y_true, y_pred):
+    tp = true_positives(y_true, y_pred)
+    fp = false_positives(y_true, y_pred)
+    return tp / (tp + fp)
 
 def positive_predictive_value(y_true, y_pred, group):
-    # PPV for every group
-    for group_value in group.unique():
-        # Get the indices for the group
-        group_indices = group == group_value
-        # Get the true positives
-        true_positives = np.logical_and(y_true[group_indices], y_pred[group_indices])
-        # Get the false positives
-        false_positives = np.logical_and(np.logical_not(y_true[group_indices]), y_pred[group_indices])
-        # Calculate the PPV
-        ppv = np.sum(true_positives) / (np.sum(true_positives) + np.sum(false_positives))
-        print(f"PPV for {group.name}={group_value}: {ppv}")
-    # get difference in PPV
-    ppv_diff = np.abs(ppv[0] - ppv[1])
-    return ppv_diff
+    #tp = np.sum(np.logical_and(y_true, y_pred))
+    #fp = np.sum(np.logical_and(np.logical_not(y_true), y_pred))
+    #return tp / (tp + fp)
+    return MetricFrame(metrics=positive_predictive_value_helper, y_true=y_true, y_pred=y_pred, sensitive_features=group).difference(method='between_groups')
     
 
 # Helper functions
@@ -60,3 +87,20 @@ def get_metrics_df(models_dict, y_true, group):
         df_dict[metric_name] = [metric_func(preds) if use_preds else metric_func(scores) 
                                 for model_name, (preds, scores) in models_dict.items()]
     return pd.DataFrame.from_dict(df_dict, orient="index", columns=models_dict.keys())
+
+
+
+
+g = ["m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","m", "m", "m","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f"] #np.concatenate((np.ones(27), np.zeros(18)), axis=0)
+print(len(g))
+y_true = list(np.concatenate((np.ones(9), np.zeros(18), np.ones(9), np.zeros(9)), axis=0))
+y_pred = list(np.concatenate((np.ones(3), np.zeros(6), np.ones(3), np.zeros(15), np.ones(2), np.zeros(7), np.ones(2), np.zeros(7)), axis=0))
+
+
+ 
+
+#df_g = pd.DataFrame(g, columns=['gender'])
+
+ppv = positive_predictive_value(y_true, y_pred, g)
+
+print(ppv)
