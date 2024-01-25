339f98a0213d7d23171c42a79d86cb2c22818b6d
linh <lvu5@uic.edu>
2024-01-25 14:23:22 -0600

    update nn code

--- spf/lib/python3.8/site-packages/torchaudio/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import (
-    sox_utils,
-)
-
-from torchaudio._internal import module_utils as _mod_utils
-
-
-if _mod_utils.is_module_available('torchaudio._torchaudio'):
-    sox_utils.set_verbosity(1)
